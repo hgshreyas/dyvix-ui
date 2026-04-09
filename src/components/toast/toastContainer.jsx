0ddb517b8720365f1496f4ab9e28332b85bd750d
younisdev <younisayoub10@gmail.com>
2026-04-10 02:04:46 +0300

    Add toast validation

--- src/components/toast/toastContainer.jsx
+++ src/components/toast/toastContainer.jsx
@@ -1,16 +1,34 @@
 import './dependencies/style/style.css';
 import positionData from './dependencies/positions.json';
 import TypesData from './dependencies/types.json';
+import animationsData from '../animations.json';
 import React from 'react';
 import { subscribe } from './bus';
 import { DyvixToastItem } from './toast';
+import { ValidateContainer } from './validation';
+import { GaurdStatus, EvaluateFailure } from '../../utils/DyvixGuard';
+
+export const validPositions = positionData.map((e) => e.position);
+export const validTypes = TypesData.map((e) => e.type);
+export const validAnimations = animationsData.map((e) => e.animation);
 
 function DyvixToastContainer({
-  position,
+  position = 'top-right',
   segments,
-  duration = 3000,
+  duration = 5000,
   animation = 'zoom'
 }) {
+  const validator = ValidateContainer(
+  position,
+  segments,
+  duration,
+  animation
+  );
+
+  if (validator.status === GaurdStatus.Error) {
+    return EvaluateFailure(validator.error, validator.status);
+  }
+
   const [toasts, setToasts] = React.useState([]);
   const currentPosition = positionData.find(
     (e) => e.position.trim().toLowerCase() === position.trim().toLowerCase()
@@ -33,9 +51,11 @@ function DyvixToastContainer({
           (e) => e.type.trim().toLowerCase() === toast.type.trim().toLowerCase()
         );
         const currentclass = `dyvix-toast ${currentType.class}`;
+        const randid = crypto.randomUUID();
+
         return (
           <DyvixToastItem
-            key={toast.id}
+            key={randid}
             message={toast.message}
             onClose={() =>
               setToasts((prev) => prev.filter((t) => t.id !== toast.id))
