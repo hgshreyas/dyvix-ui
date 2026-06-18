31ca0bc7a304246175d249c37cb960ea180abc9f
younisdev <younisayoub10@gmail.com>
2026-06-19 00:28:31 +0300

    Fix modal submit being blocked by async stale state.

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -148,13 +148,13 @@ function Modal({
       }
     }
     SetErrors(newErrors);
+    return newErrors;
   }
   function handleSubmit() {
-    const validation = handleValidation(data);
+    const newErrors = handleValidation(data);
     const allow =
-      Object.values(errors).every((val) => val === null) &&
-      Object.keys(errors).length > 0;
-
+      Object.values(newErrors).every((val) => val === null) &&
+      Object.keys(data).some((key) => data[key] !== null);
     if (typeof onSubmit === 'function' && allow) {
       onSubmit(data);
     }
@@ -465,7 +465,7 @@ function Modal({
             {currentType.submit && (
               <DyvixButton
                 className="modal-btn"
-                onClick={() => handleSubmit}
+                onClick={handleSubmit}
                 theme={theme.toLowerCase()}
               >
                 {currentType.submitLabel}
