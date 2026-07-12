1bdef4f0f8b4a835c416c2581b2bb621fa09f823
Gautami1407 <gouthaminurchu@gmail.com>
2026-07-12 23:12:02 +0530

    Add Coffee theme to button registry

--- devbench/src/component/button.jsx
+++ devbench/src/component/button.jsx
@@ -1,9 +1,37 @@
-import { DyvixButton } from 'dyvix-ui';
-import { DYVIX_GLOBAL_THEME } from 'dyvix-ui';
+import React from 'react';
+import { DyvixButton, DYVIX_GLOBAL_THEME } from 'dyvix-ui';
+
 export function ButtonTest() {
+  const [theme, setTheme] = React.useState(DYVIX_GLOBAL_THEME.SINGULARITY);
+
+  const themeOptions = React.useMemo(
+    () => Object.values(DYVIX_GLOBAL_THEME),
+    []
+  );
+
   return (
     <>
-      <DyvixButton onClick={() => console.log('clicked')}>Submit</DyvixButton>
+      <div style={{ display: 'flex', gap: 12, alignItems: 'center' }}>
+        <label htmlFor="dyvix-button-theme">Theme</label>
+        <select
+          id="dyvix-button-theme"
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+        >
+          {themeOptions.map((t) => (
+            <option key={t} value={t}>
+              {t}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div style={{ marginTop: 16 }}>
+        <DyvixButton theme={theme} onClick={() => console.log('clicked')}>
+          Submit
+        </DyvixButton>
+      </div>
     </>
   );
 }
+
