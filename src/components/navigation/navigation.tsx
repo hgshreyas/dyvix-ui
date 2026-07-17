6f49c2af1c493dc292b19b49d88215964352699b
younisdev <younisayoub10@gmail.com>
2026-07-17 19:23:12 +0300

    Link sub-nav component to parent component.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,9 +1,13 @@
 import React, { type FC, type ReactNode } from 'react';
 import './dependencies/style/style.css';
 import { ConstructClasses } from '../../utils/utils';
+import DyvixNavBrand from './DyvixNavBrand';
+import DyvixNavLink from './DyvixNavLink';
+import DyvixNavMenu from './DyvixNavMenu';
 import { EvaluateFailure, GuardStatus } from '../../utils/DyvixGuard';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+
 
 interface DyvixNavProps {
   children?: ReactNode,
@@ -11,7 +15,13 @@ interface DyvixNavProps {
   animation: 'fade'|'bubble'|'zoom'|'unfold'|'glitch'|'pulse'|'aurora'|'drop'|'flip'|'glide'|'drift'|'float'|'swing'
 }
 
-const DyvixNav: FC<DyvixNavProps> = ({ children,  className, animation="fade" }) => {
+interface DyvixNavComponents extends FC<DyvixNavProps> {
+  Menu: typeof DyvixNavMenu,
+  Brand: typeof DyvixNavBrand,
+  Link: typeof DyvixNavLink
+}
+
+const DyvixNav: DyvixNavComponents = ({ children,  className, animation="fade" }) => {
   return (
     <div className='dyvix-nav-wrapper'>
       <nav className={ConstructClasses('dyvix-nav', className)}>
@@ -20,5 +30,9 @@ const DyvixNav: FC<DyvixNavProps> = ({ children,  className, animation="fade" })
     </div>
   )
 }
+
+DyvixNav.Menu = DyvixNavMenu;
+DyvixNav.Brand = DyvixNavBrand;
+DyvixNav.Link = DyvixNavLink;
 
 export default DyvixNav;
